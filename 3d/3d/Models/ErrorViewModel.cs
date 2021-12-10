29b000b4f5aaa144812942cd513b0420baafc315
hieuhp197 <hieuhp197@gmail.com>
2021-12-10 17:51:34 +0700

    ud1

---
+++ 3d/3d/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _3d.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
